31bbd8bf017e0565b8b3035e77dda83813d79e65
plusplus7 <jackiedeng.cn@gmail.com>
2018-01-10 00:10:21 +0800

    Added navbar.

--- Cirno5/web/src/components/Welcome.css.d.ts
+++
@@ -1,2 +0,0 @@
-export const welcome: string;
-export const motherfucker: string;
--- Cirno5/web/src/pages/AppPage.tsx
+++ Cirno5/web/src/pages/AppPage.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Welcome } from '../components/Welcome';
+import { NavbarComponent } from '../components/Navbar/NavbarComponent';
 import { Switch, Route, Link } from 'react-router-dom';
 import { ArticleListPage } from './ArticleListPage';
 import { ArticlePage } from './ArticlePage';
@@ -8,12 +8,7 @@ export class AppPage extends React.Component {
     public render() {
         return (
             <div>
-                <ul>
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/blog'>Blog</Link></li>
-                    <li><Link to='/storage'>Storage</Link></li>
-                    <li><Link to='/about'>About</Link></li>
-                </ul>
+                <NavbarComponent />
                 <Switch>
                     <Route exact path='/blog' render={() => <ArticleListPage />} />
                     <Route path='/blog/:key' render={(props) => <ArticlePage link={props.match.params.key} />} />
